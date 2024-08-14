99241b6f4d6793ee9be24b2206d00c5ee47b2fe2
John Lundy <johnclundyy@gmail.com>
2024-08-14 12:22:31 -0600

    Added Admin

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,15 +4,24 @@ import Cards from "../ProjectCard/ProjectCards";
 const projects = [
   {
     id: 1,
+    title: "Verdant Vision Admin",
+    description:
+      "A robust SAAS addon to Verdant Visions Landscaping, providing a comprehensive admin dashboard for real-time quote management and client communication. Built with React and Material UI on the front-end, and AWS-powered back-end services for seamless, live interactions. This solution exemplifies expertise in serverless architecture, cloud integration, and efficient client management systems.",
+    getImageSrc: () => require("./verdant-admin.png"),
+    github: "https://github.com/johnlundy94/landscape-admin",
+    website: "https://verdantvisionslandscapingadmin.com/",
+  },
+  {
+    id: 2,
     title: "Verdant Vision Landscaping",
     description:
       "A sleek interface for landscape services, offering design visualization and quote submission with an AWS-powered back-end. Demonstrates understanding in cloud solutions, serverless architecture, and database scalability.",
     getImageSrc: () => require("./vvl.png"),
     github: "https://github.com/johnlundy94/landscape",
-    website: "https://www.verdantvisionslandscaping.com/",
+    website: "https://verdantvisionslandscapingadmin.com/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Calorie Counter",
     description:
       "A user-friendly app providing tailored nutritional tracking, with Firebase-backend authentication and live data updates. Simplifies daily nutritional management through an intuitive questionnaire-based setup.",
@@ -21,7 +30,7 @@ const projects = [
     website: "https://www.johnscaloriecounter.com/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Little Lemon",
     description:
       "Merges appealing design with a reliable reservation system, ensuring seamless user experiences and precision in booking management to prevent overlaps.",
