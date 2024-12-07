f53c5b0d3e6f320e9b3497ba66da0d15593a3393
John Lundy <johnclundyy@gmail.com>
2024-12-07 15:59:20 -0700

    Sprused up the background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import "./App.css";
+import { useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import muiTheme from "./muiTheme";
 import Home from "./Home";
 
 function App() {
+  useEffect(() => {
+    window.particlesJS("particles-js", {
+      particles: {
+        number: { value: 100, density: { enable: true, value_area: 800 } },
+        color: { value: "#ADEFD1" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5, random: true },
+        size: { value: 3, random: true },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ADEFD1",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: { enable: true, speed: 0.5, random: true },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: { enable: true, mode: "grab" },
+          onclick: { enable: true, mode: "push" },
+        },
+        modes: {
+          grab: { distance: 200, line_linked: { opacity: 0.7 } },
+          push: { particles_nb: 4 },
+        },
+      },
+      retina_detect: true,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <ThemeProvider theme={muiTheme}>
-        <Home />
-      </ThemeProvider>
+    <div>
+      <div id="particles-js"></div>
+
+      <div className="App">
+        <ThemeProvider theme={muiTheme}>
+          <Home />
+        </ThemeProvider>
+      </div>
     </div>
   );
 }
